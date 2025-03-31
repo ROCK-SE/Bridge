637bc7da5f11bfd1f0ec721d08a8d7e9241f00f1
Kai Gao <gaokaipku@qq.com>
2025-03-31 10:42:08 +0800

    Parse all dependency configuration files.

--- benchmark_construction/parser.py
+++ benchmark_construction/parser.py
@@ -1,81 +1,146 @@
 import argparse
+import json
+import logging
+import math
 import os
+import sys
 
+import pandas as pd
+from joblib import Parallel, delayed
 from parse_pom import parse_pom
+from parse_pyproject import parse_pyproject_toml
 from parse_requirements import parse_requirements
-from woc.local import WocMapsLocal
+from parse_setup_cfg import parse_setup_cfg
+from parse_setup_py import parse_setup_py
+from tqdm import tqdm
+from utils import read_blob
 
-woc = WocMapsLocal()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+PARSERS = {
+    "setup.cfg": parse_setup_cfg,
+    "pyproject.toml": parse_pyproject_toml,
+    "requirements.txt": parse_requirements,
+    "pom.xml": parse_pom,
+    "setup.py": parse_setup_py,
+}
 
 
-def read_blob(sha: str) -> str | None:
-    """Read a blob's content by it sha1 value
-
-    Parameters
-    ----------
-    sha : str
-        a sha1 hash string containing 40 hexadecimal digits
-
-    Returns
-    -------
-    str | None
-        the blob's content or None if an error occurs.
-    """
-    try:
-        data = woc.show_content("blob", sha)
-    except:
-        data = None
-    return data
+def split_blobs(blob_shas: list[str], batch_size: int = 1):
+    for i in range(0, len(blob_shas), batch_size):
+        yield blob_shas[i : i + batch_size]
 
 
 # Function to process the blob hash and choose the type of parsing (POM or requirements.txt)
-def process_blob(blob_hash: str, parse_type: str):
-    """Process the specified blob hash and parse either POM or requirements.txt"""
-    # Fetch the content of the blob
-    file_content = read_blob(blob_hash)
-
-    if file_content:
-        print(f"Processing blob: {blob_hash}")
-
-        if parse_type == "pom":
-            # Parse the POM file and output dependencies
-            packages = parse_pom(file_content)
-
-            # Output parsed results
-            for package in packages:
-                print(f"Package: {package['name']}, Version: {package['version']}")
-
-        elif parse_type == "requirements":
-            # Parse the requirements.txt content and output dependencies
-            dependencies = parse_requirements(file_content)
-
-            # Output parsed results
-            for package, version in dependencies.items():
-                print(f"Package: {package}, Version: {version}")
-
+def single_process(
+    blobs: list[str], configuration_file_type: str, save_path: str, idx: int
+):
+    result = {}
+    missing = []
+    parser = PARSERS[configuration_file_type]
+    for blob in blobs:
+        content = read_blob(blob)
+        if content is None:
+            missing.append(blob)
         else:
-            print("Invalid parse type. Please choose either 'pom' or 'requirements'.")
-    else:
-        print(f"Failed to fetch content for blob {blob_hash}")
+            result[blob] = parser(content)
+    logger.error(f"Start dumping results for {configuration_file_type} batch {idx}...")
+    with open(f"{save_path}.json.{idx}", "w") as outf:
+        json.dump(result, outf)
+    with open(f"{save_path}.missing.{idx}", "w") as outf:
+        for b in missing:
+            outf.write(f"{b}\n")
 
 
-# Main program
+def main(
+    configuration_file_type: str, directory: str, num_workers: int, batch_size: int
+):
+    print(f"{configuration_file_type}: ")
+    source_path = os.path.join(
+        directory, "commits", f"{configuration_file_type}_commits.csv"
+    )
+    save_path = os.path.join(
+        directory, "deps", f"{configuration_file_type}_dependencies"
+    )
+    os.makedirs(os.path.join(directory, "deps"), exist_ok=True)
+
+    df = pd.read_csv(source_path, keep_default_na=False, low_memory=False)
+    blob_shas = list(set(list(df["new blob"]) + list(df["old blob"])))
+    print(f"\t{len(blob_shas)} unique blobs")
+    del df
+    blob_shas.sort()
+    num_batches = math.ceil(len(blob_shas) / batch_size)
+    print(f"\tnumber of batches: {num_batches}")
+
+    batches = split_blobs(blob_shas, batch_size)
+    Parallel(n_jobs=num_workers)(
+        delayed(single_process)(blobs, configuration_file_type, save_path, i)
+        for i, blobs in enumerate(tqdm(batches, total=num_batches, file=sys.stdout))
+    )
+
+    result = {}
+    missing = []
+    for i in range(num_batches):
+        with open(f"{save_path}.json.{i}") as inf:
+            data = json.load(inf)
+            for k, v in data.items():
+                result[k] = v
+        os.remove(f"{save_path}.json.{i}")
+        
+        with open(f"{save_path}.missing.{i}") as inf:
+            missing.extend(inf.read().splitlines())
+        os.remove(f"{save_path}.missing.{i}")
+
+    with open(f"{save_path}.json", "w") as outf:
+        json.dump(result, outf)
+    with open(f"{save_path}.missing", "w") as outf:
+        for b in missing:
+            outf.write(f"{b}\n")
+
+
 if __name__ == "__main__":
-    # Directly declare the lookup tool path
-    lookup_path = "~/lookup"  # Replace with the actual lookup tool path
-
     # Command-line arguments for blob hash and parse type
     parser = argparse.ArgumentParser(
-        description="Parse blob content to extract dependencies"
+        prog="python parser.py",
+        description="Parse configuration blobs to extract dependencies",
     )
-    parser.add_argument("blob_hash", type=str, help="The blob hash to process")
     parser.add_argument(
-        "parse_type",
-        choices=["pom", "requirements"],
-        help="Type of file to parse ('pom' or 'requirements')",
+        "-t", "--configuration_file_type", help="type of configuration file"
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        type=str,
+        help="the directory to read commits and save results",
+    )
+    parser.add_argument(
+        "-n", "--num_workers", type=int, default=1, help="number of threads"
+    )
+    parser.add_argument(
+        "-b",
+        "--batch_size",
+        type=int,
+        default=1,
+        help="number of blobs for each thread to process per batch",
     )
 
     args = parser.parse_args()
 
-    # Process the blob hash and parse based on the specified type
-    process_blob(args.blob_hash, lookup_path, args.parse_type)
+    cft = args.configuration_file_type
+    if cft == "all":
+        for t in PARSERS.keys():
+            main(
+                t,
+                args.directory,
+                args.num_workers,
+                args.batch_size,
+            )
+
+    elif cft in PARSERS:
+        main(
+            cft,
+            args.directory,
+            args.num_workers,
+            args.batch_size,
+        )
