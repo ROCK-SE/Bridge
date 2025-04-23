e8060614c99b82b54c303427aa727c74d4b89cfe
Kai Gao <gaokaipku@qq.com>
2025-04-24 01:09:08 +0800

    Separate Python blobs and Java blobs for easier API call analysis.

--- benchmark_construction/extract_blob.sh
+++ benchmark_construction/extract_blob.sh
@@ -20,19 +20,30 @@ rm all_commits
 echo $(cat c2fbb | wc -l) records in c2fbb
 # 139,389,700 records in c2fbb
 
-# Get all unique blobs in obtained c2fbb
-cut -d\; -f3,4 c2fbb | tr -s ';' '\n' | sort -u >all_blobs
-echo $(cut -d\; -f1 c2fbb | sort -u | wc -l) commits modified $(cat all_blobs | wc -l) java/python blobs
-# 7,330,749 commits modified 62,246,912 java/python blobs
+# Get all unique Python and Java blobs in obtained c2fbb
+cat c2fbb | grep ".py;" | cut -d\; -f3,4 | tr -s ';' '\n' | sort -u >pyblobs
+echo $(cat c2fbb | grep ".py;" | cut -d\; -f1 | sort -u | wc -l) commits modified $(cat pyblobs | wc -l) python blobs
+# 3,500,391 commits modified 15,890,764 python blobs
+cat c2fbb | grep ".java;" | cut -d\; -f3,4 | tr -s ';' '\n' | sort -u >javablobs
+echo $(cat c2fbb | grep ".java;" | cut -d\; -f1 | sort -u | wc -l) commits modified $(cat javablobs | wc -l) java blobs
+# 3,907,437 commits modified 46,356,165 java blobs
+# 7,330,749 commits modified 62,246,912 java blobs
 
-# Split all blobs into 128 sections
+# Split all Python and Java blobs into 128 sections
 cd -
-python split_shas.py -f all_blobs -c 0 -p blob -n 128
+python split_shas.py -f pyblobs -c 0 -p pyblob -n 128
+python split_shas.py -f javablobs -c 0 -p javablob -n 128
 
-for s in {0..31}; do
-    for j in {0..3}; do
-        i=$((j + s * 4))
-        cat /data/All.blobs/blob_$i.idx | awk -F\; '{if(NF>5){print $5";"$2";"$3}else{print $4";"$2";"$3}}' | sort -t\; -T. -S 15G -k1,1 | join -t\; -o 1.1 1.2 1.3 - blob.$i >blob_$i.idx &
+# Get the offset and length of each blob object
+cd ../benchmark/updates
+for p in {py,java}; do
+    for s in {0..31}; do
+        for j in {0..3}; do
+            i=$((j + s * 4))
+            cat /data/All.blobs/blob_$i.idx | awk -F\; '{if(NF>5){print $5";"$2";"$3}else{print $4";"$2";"$3}}' | sort -t\; -T. -S 15G -k1,1 | join -t\; -o 1.1 1.2 1.3 - ${p}blob.$i >${p}blob_$i.idx &
+        done
+        wait
     done
-    wait
 done
+rm pyblob.{0..127}
+rm javablob.{0..127}
