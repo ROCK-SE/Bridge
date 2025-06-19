94f4b194199fc87bda7b4058a755ca850eb8cdab
Kai Gao <gaokaipku@qq.com>
2025-06-19 23:48:02 +0800

    More precise API calls Parser for Java. 1. Consider more variable declaration statements including `instanceof_expression`, `type_pattern`, `record_pattern_component`, `catch_formal_parameter`. 2. Fix API call Chain extraction erros in the field access case. 3. Remove the unnecessary return value `full_class_names` in the `parse_imports_java` function. 4. Consider more context types for caller extraction including `constructor_declaration`, `compact_constructor_declaration`, `record_declaration`, `enum_declaration`, `interface_declaration`, and `annotation_type_declaration`. 5. Filter `annotated_type` and `array_type` when parsing variable types and extract the `name` field and ignore the type parameter. 6. Refactor the result schema for the convenience of dumping to MongoDB.

--- benchmark_construction/parse_api_calls.py
+++ benchmark_construction/parse_api_calls.py
@@ -1,10 +1,11 @@
+import itertools
 import json
 import logging
 import math
 import os
 import sys
 
-# import random
+
 from argparse import ArgumentParser
 
 import tree_sitter_java as tsjava
@@ -27,7 +28,13 @@ java_parser = Parser(JAVA_LANGUAGE)
 
 if not os.path.exists("java_standard_packages.json"):
     java_package_list()
-JAVA_STDLIB = json.load(open("java_standard_packages.json"))["all"]
+JAVA_STDLIB = list(
+    set(
+        itertools.chain.from_iterable(
+            json.load(open("java_standard_packages.json")).values()
+        )
+    )
+)
 
 py_import_query = PY_LANGUAGE.query(
     """
@@ -62,29 +69,64 @@ py_call_query = PY_LANGUAGE.query(
 java_import_query = JAVA_LANGUAGE.query(
     """
 (import_declaration
-  (identifier) @import_name . )
+    (identifier) @import_name .)
 (import_declaration
-  (scoped_identifier) @import_name . )
+    (scoped_identifier) @import_name .)
 """
 )
 
-java_variale_declaration_query = JAVA_LANGUAGE.query(
+java_variable_declaration_query = JAVA_LANGUAGE.query(
     """
-[(field_declaration)
- (local_variable_declaration)
- (resource)
- (enhanced_for_statement)
- (constant_declaration)
- (formal_parameter)] @declaration
+(instanceof_expression
+    right: (_) @type
+    name: (_) @identifier) @variable_declaration
+(type_pattern
+    (_) @type
+    (_) @identifier) @variable_declaration
+(record_pattern_component
+    (_) @type
+    (_) @identifier) @variable_declaration
+(catch_formal_parameter
+    (catch_type . (_) @type)
+    name: (_) @identifier
+    !dimensions) @variable_declaration
+(resource
+    type: (_) @type
+    name: (_) @identifier
+    !dimensions) @variable_declaration
+(enhanced_for_statement
+    type: (_) @type
+    name: (_) @identifier
+    !dimensions) @variable_declaration
+(field_declaration
+    type: (_) @type
+    (variable_declarator
+        name: (_) @identifier
+        !dimensions)) @variable_declaration
+(formal_parameter
+    type: (_) @type
+    name: (_) @identifier
+    !dimensions) @variable_declaration
+(constant_declaration
+    type: (_) @type
+    (variable_declarator
+        name: (_) @identifier
+        !dimensions)) @variable_declaration
+(local_variable_declaration
+    type: (_) @type
+    (variable_declarator
+        name: (_) @identifier
+        !dimensions)) @variable_declaration
 """
 )
 
 java_call_query = JAVA_LANGUAGE.query(
     """
 (method_invocation
-  object: (identifier)? @object
-  name: (identifier) @name
-  arguments: (argument_list) @arguments)@method_invocation
+    object: (_)? @object
+    .
+    name: (_) @name
+    arguments: (argument_list) @arguments)@method_invocation
 """
 )
 
@@ -120,7 +162,7 @@ def resolve_alias_name(name: str, alias_mapping: dict[str, str]) -> str | None:
     return None
 
 
-def get_context_py(cur_node: Node):
+def get_caller_py(cur_node: Node):
     parent = cur_node.parent
     context = []
     while parent:
@@ -193,7 +235,7 @@ def parse_api_calls_python(source: bytes | str):
             arguments.append(arg_info)
 
         cur_node = match[1]["call"][0]
-        caller = get_context_py(cur_node)
+        caller = get_caller_py(cur_node)
 
         line_no = match[1]["name"][0].start_point[0]
         call_graphs[caller] = call_graphs.get(caller, [])
@@ -208,24 +250,22 @@ def parse_api_calls_python(source: bytes | str):
 
 
 def parse_imports_java(tree: Tree):
-    full_class_names = []
     class_mappings = {}
     for match in java_import_query.matches(tree.root_node):
         import_name = match[1]["import_name"][0].text.decode(errors="ignore")
         if any(import_name.startswith(f"{p}.") for p in JAVA_STDLIB):
             continue
-        full_class_names.append(import_name)
         class_mappings[import_name.split(".")[-1]] = import_name
-    return full_class_names, class_mappings
+    return class_mappings
 
 
-def get_context_java(cur_node: Node):
+def get_caller_java(cur_node: Node):
     parent = cur_node.parent
     context = []
     while parent:
         if parent.type == "program":
             break
-        elif parent.type == "method_declaration":
+        elif parent.type in ["method_declaration", "constructor_declaration"]:
             method_name = parent.child_by_field_name("name").text.decode(
                 errors="ignore"
             )
@@ -236,60 +276,49 @@ def get_context_java(cur_node: Node):
                     t = param.child_by_field_name("type").text.decode(errors="ignore")
                     parameter_types.append(t)
             context.append(f"{method_name}({', '.join(parameter_types)})")
-        elif parent.type == "class_declaration":
+        elif parent.type == "compact_constructor_declaration":
+            context.append(
+                parent.child_by_field_name("name").text.decode(errors="ignore")
+            )
+        elif parent.type in [
+            "class_declaration",
+            "record_declaration",
+            "enum_declaration",
+            "interface_declaration",
+            "annotation_type_declaration",
+        ]:
             class_name = parent.child_by_field_name("name").text.decode(errors="ignore")
-            context.append(class_name)
+            context.append(f"{parent.type.split('_')[0]}@{class_name}")
         parent = parent.parent
 
     return tuple(reversed(context))
 
 
-def parse_variable_types_java(
-    tree: Tree, full_class_names: list[str] = [], class_mappings: dict[str, str] = {}
-):
+def parse_variable_types_java(tree: Tree):
     variable_types = {}
-    for match in java_variale_declaration_query.matches(tree.root_node):
-        declaration = match[1]["declaration"][0]
-        line_no = declaration.start_point[0]
-        t_node = declaration.child_by_field_name("type")
-        if t_node is None:
-            continue
-        if t_node.type in ["identifier", "type_identifier"]:
-            t = t_node.text.decode(errors="ignore")
-        elif (t_node.type == "generic_type") and (
-            t_node.child(0).type in ["identifier", "type_identifier"]
-        ):
-            t = t_node.child(0).text.decode(errors="ignore")
-        else:
-            continue
-        if (t not in class_mappings) and (t not in full_class_names):
-            continue
-        context = get_context_java(declaration)
+    for match in java_variable_declaration_query.matches(tree.root_node):
+        declaration_node = match[1]["variable_declaration"][0]
+        line_no = declaration_node.start_point[0]
 
-        names = []
-        if declaration.type in [
-            "resource",
-            "enhanced_for_statement",
-            "formal_parameter",
-        ]:
-            names = [
-                declaration.child_by_field_name("name").text.decode(errors="ignore")
-            ]
-        for variable_declarator in declaration.children_by_field_name("declarator"):
-            names.append(
-                variable_declarator.child_by_field_name("name").text.decode(
-                    errors="ignore"
-                )
-            )
-        for name in names:
-            variable_types[name] = variable_types.get(name, {})
-            if context not in variable_types[name]:
-                variable_types[name][context] = [(line_no, t)]
-            else:
-                variable_types[name][context].append((line_no, t))
-    for name, context in variable_types.items():
-        for k, v in context.items():
-            context[k] = sorted(v, key=lambda x: x[0])
+        type_node = match[1]["type"][0]
+        if type_node.type in ["annotated_type", "array_type"]:
+            continue
+        if type_node.type == "generic_type":
+            type_str = type_node.child(0).text.decode(errors="ignore")
+        else:
+            type_str = type_node.text.decode(errors="ignore")
+
+        identifier_str = match[1]["identifier"][0].text.decode(errors="ignore")
+        if identifier_str == "_":
+            continue
+
+        context = get_caller_java(declaration_node)
+        variable_types[identifier_str] = variable_types.get(identifier_str, {})
+        if context not in variable_types[identifier_str]:
+            variable_types[identifier_str][context] = [(line_no, type_str)]
+        else:
+            variable_types[identifier_str][context].append((line_no, type_str))
+
     return variable_types
 
 
@@ -297,16 +326,14 @@ def resolve_obj_type_java(
     obj_name: str,
     context: tuple[str],
     line_no: int,
-    variable_types: dict[str, dict[str, list[tuple[str]]]],
+    variable_types: dict[str, dict[str, list[tuple[int, str]]]],
 ) -> str | None:
     if obj_name not in variable_types:
         return obj_name
     context_type_info = variable_types.get(obj_name)
-    for i in range(len(context)):
-        if i == 0:
-            tmp_context = context[:]
-        else:
-            tmp_context = context[:-i]
+    context_len = len(context)
+    for i in range(context_len):
+        tmp_context = context[: context_len - i]
         if tmp_context in context_type_info:
             res = context_type_info[tmp_context][0][1]
             for l, t in context_type_info[tmp_context][1:]:
@@ -323,36 +350,61 @@ def parse_api_calls_java(source: bytes | str):
 
     tree = java_parser.parse(source)
 
-    full_class_names, class_mappings = parse_imports_java(tree)
-    variable_types = parse_variable_types_java(tree, full_class_names, class_mappings)
+    class_mappings = parse_imports_java(tree)
+    variable_types = parse_variable_types_java(tree)
 
     call_graphs = {}
     for match in java_call_query.matches(tree.root_node):
-        cur_node = match[1]["method_invocation"][0]
-        line_no = cur_node.start_point[0]
-        context = get_context_java(cur_node)
-        name = match[1]["name"][0].text.decode(errors="ignore")
-        arg_node = match[1]["arguments"][0]
-        args = [child.text.decode(errors="ignore") for child in arg_node.named_children]
+        method_name = match[1]["name"][0].text.decode(errors="ignore")
         if "object" in match[1]:
-            obj_name = match[1]["object"][0].text.decode(errors="ignore")
-            obj_type = resolve_obj_type_java(obj_name, context, line_no, variable_types)
-            if obj_type in class_mappings:
-                obj_type = class_mappings[obj_type]
-            if not obj_type in full_class_names:
-                continue
-            name = f"{obj_type}.{name}"
-        else:
-            if name in class_mappings:
-                name = class_mappings[name]
-            if not name in full_class_names:
+            obj_node = match[1]["object"][0]
+            if obj_node.type in ["identifier", "_reserved_identifier", "field_access"]:
+                obj_name = obj_node.text.decode(errors="ignore")
+                method_name = f"{obj_name}.{method_name}"
+            else:
                 continue
 
-        if context not in call_graphs:
-            call_graphs[context] = []
-        call_graphs[context].append([name, line_no, args])
-    call_graphs = [[k, v] for k, v in call_graphs.items()]
-    return [full_class_names, call_graphs]
+        parts = method_name.split(".")
+        left_part, right_parts = parts[0], parts[1:]
+        if (left_part not in variable_types) and (left_part not in class_mappings):
+            continue
+        cur_node = match[1]["method_invocation"][0]
+        line_no = cur_node.start_point[0]
+        context = get_caller_java(cur_node)
+        if left_part in variable_types:
+            left_part = resolve_obj_type_java(
+                left_part, context, line_no, variable_types
+            )
+        if left_part not in class_mappings:
+            continue
+        left_part_type = class_mappings.get(left_part)
+        method_name = ".".join([left_part_type] + right_parts)
+
+        arguments_node = match[1]["arguments"][0]
+        arguments = []
+        for arg_node in arguments_node.named_children:
+            arg_value = arg_node.text.decode(errors="ignore")
+            arg_type = arg_node.type
+            if arg_type == "identifier":
+                arg_type = resolve_obj_type_java(
+                    arg_value, context, line_no, variable_types
+                )
+            arguments.append(
+                {
+                    "value": arg_value,
+                    "value_type": arg_type,
+                }
+            )
+
+        call_graphs[context] = call_graphs.get(context, [])
+        call_graphs[context].append(
+            {"full_name": method_name, "line_no": line_no, "arguments": arguments}
+        )
+
+    return {
+        "modules": list(class_mappings.values()),
+        "api_calls": [{"caller": k, "callee": v} for k, v in call_graphs.items()],
+    }
 
 
 def read_raw_blob(idx: int, offset: int, length: int) -> bytes:
@@ -363,7 +415,6 @@ def read_raw_blob(idx: int, offset: int, length: int) -> bytes:
 
 def generate_batch(lang: str, batch_size: int):
     batch = []
-    # cnt = 0
     with open(f"../benchmark/updates/{lang}blob.idx") as f:
         for line in f:
             blob_sha, offset, length = line.strip("\n").split(";")
@@ -400,7 +451,6 @@ def parse_batch(lang: str, i: int, batch: list):
 
 def parse_all(lang: str, n_jobs: int = 1, batch_size: int = 1):
     total_lines = sum(1 for i in open(f"../benchmark/updates/{lang}blob.idx", "rb"))
-    # total_lines = 10000
     num_batches = math.ceil(total_lines / batch_size)
     print(
         f"{lang}: {n_jobs} processes",
