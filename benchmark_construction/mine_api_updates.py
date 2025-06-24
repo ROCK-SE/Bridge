30d1cb0336503f249a0b46ee5efc6895a43d9307
Kai Gao <gaokaipku@qq.com>
2025-06-25 00:12:38 +0800

    Extract APIs relevant to dependency update. The procedure is as follows: 1. Obtain the changed configuration filepath and corresponding updated packages (name, top level modules, version before change, and version after change) for each dependency update commit. 2. For each dependency update commit and their previsou 2 and after 2 commits, obtain their changed filepath and blobs before and after change. 3. Based on the changed filepath, select the nearest changed dependency configuration filepath and use updated packages in it to select API calls to the updated changes in the blobs. 4. Remove API calls in the same caller with the same full qualified name and the same number of arguments. 5. Save the results per changed blob pair and per package.

--- benchmark_construction/mine_api_updates.py
+++ benchmark_construction/mine_api_updates.py
@@ -1,243 +1,329 @@
 import argparse
 import json
+import logging
 import math
+import os
+import sys
 
+import pandas as pd
 from joblib import Parallel, delayed
 from Levenshtein import distance, ratio
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from tqdm.auto import tqdm
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler("../log/mine_api_updates.log", mode="w")
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+# add the handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
+
 py_imports = json.load(open("../benchmark/updates/py_imports.json"))
 java_imports = json.load(open("../benchmark/updates/java_imports.json"))
+lang_pkg_import_mappings = {"py": py_imports, "java": java_imports}
 
 
-def check_imports(
-    import_modules: list[str], package_imports: dict[str, list[str]]
-) -> dict[str, list[str]]:
+def get_dependency_update_info(
+    dep_updates_col: Collection, commit: str, pkg_import_mappings: dict[str, list[str]]
+) -> list[dict]:
+    dep_update_info = []
+    for doc in dep_updates_col.find(
+        {"commit": commit},
+        projection={"_id": 0, "filepath": 1, "updated_packages": 1},
+    ):
+        pkg_info = []
+        for pkg in doc["updated_packages"]:
+            import_list = pkg_import_mappings.get(pkg["name"])
+            if import_list is None:
+                continue
+            pkg_info.append(
+                {
+                    "name": pkg["name"],
+                    "top_levels": import_list,
+                    "version_before": pkg["version_before"],
+                    "version_after": pkg["version_after"],
+                }
+            )
+        dep_update_info.append(
+            {
+                "config_file_path": doc["filepath"].strip("/"),
+                "dependency_update_info": pkg_info,
+            }
+        )
+    return dep_update_info
+
+
+def same_folder(filepath: str, config_file_path: str):
+    common_path = os.path.commonpath([filepath, config_file_path])
+    # We skip the case that filepath is identical to config_file_path
+    if common_path == os.path.dirname(config_file_path):
+        return True
+    return False
+
+
+def relevant_packages_by_filepath(
+    filepath: str, dep_update_info: list[dict]
+) -> dict[str, dict]:
+    relevant_config_file = ""
+    tmp = []
+    for dui in dep_update_info:
+        config_file_path = dui["config_file_path"]
+        if not same_folder(filepath, config_file_path):
+            continue
+        if len(config_file_path) >= len(relevant_config_file):
+            relevant_config_file = config_file_path
+            tmp = dui["dependency_update_info"]
+
+    relevant_packages = {}
+    for pkg_info in tmp:
+        name = pkg_info["name"]
+        relevant_packages[name] = {
+            k: pkg_info[k] for k in ["top_levels", "version_before", "version_after"]
+        }
+    return relevant_packages
+
+
+def get_imported_relevant_packages(
+    modules: list[str], relevant_packages: dict[str, dict]
+) -> dict[str, str]:
+    imported_relevant_packages = {}
+    for module in modules:
+        pkg = None
+        for pkg_name, pkg_info in relevant_packages.items():
+            top_levels = pkg_info["top_levels"]
+            for tl in top_levels:
+                if module.startswith(f"{tl}.") or (module == tl):
+                    pkg = pkg_name
+                    break
+            if pkg:
+                break
+        if pkg:
+            imported_relevant_packages[module] = pkg
+    return imported_relevant_packages
+
+
+def group_api_calls_by_packages(
+    api_calls: list[dict], imported_relevant_packages: dict[str, tuple[str]]
+) -> dict[str | tuple, dict[str, dict[str, list[dict]]]]:
     res = {}
-    if not import_modules:
-        return res
-    for package, imports in package_imports.items():
-        for i in imports:
-            for im in import_modules:
-                if (im.startswith(f"{i}.")) or (im == i):
-                    res[package] = res.get(package, [])
-                    res[package].append(im)
-    return res
-
-
-def filter_relevant_api_calls(
-    api_calls: list[dict], modules: list[str]
-) -> dict[str | tuple[str], list[list]]:
-    res = {}
-    for call_pair in api_calls:
-        caller = call_pair["caller"]
+    for api_call in api_calls:
+        caller = api_call["caller"]
         if isinstance(caller, list):
             caller = tuple(caller)
-        callees = call_pair["callee"]
-        for api, line_no, params in callees:
-            if any(
-                api.startswith(f"{module}.") or (api == module) for module in modules
-            ):
-                res[caller] = res.get(caller, [])
-                res[caller].append([api, line_no, params])
+        for callee in api_call["callee"]:
+            full_name = callee["full_name"]
+            for module, pkg in imported_relevant_packages.items():
+                if full_name.startswith(f"{module}.") or (full_name == module):
+                    res[caller] = res.get(caller, dict())
+                    res[caller][pkg] = res[caller].get(pkg, [])
+                    res[caller][pkg].append(callee)
+                    break
     return res
 
 
-def candidate_update_instances(
-    new_api_calls: list, old_api_calls: list, line_range: int = 2
-) -> list:
-    update_instance_candidates = []
-    new_lineno_apicalls = {}
-    for api, line_no, params in new_api_calls:
-        new_lineno_apicalls[line_no] = new_lineno_apicalls.get(line_no, [])
-        new_lineno_apicalls[line_no].append((api, params))
+def get_grouped_relevant_api_calls(
+    api_calls_col: Collection, blob: str, relevant_packages: dict[str, dict]
+) -> dict[str | tuple, dict[str, dict[str, list[dict]]]]:
+    apis_calls_info = api_calls_col.find_one(
+        {"blob": blob}, projection={"_id": 0, "modules": 1, "api_calls": 1}
+    )
+    if apis_calls_info is None:
+        return {}
+    imported_relevant_packages = get_imported_relevant_packages(
+        apis_calls_info["modules"], relevant_packages
+    )
+    if not imported_relevant_packages:
+        return {}
+    grouped_api_calls = group_api_calls_by_packages(
+        apis_calls_info["api_calls"], imported_relevant_packages
+    )
+    return grouped_api_calls
 
-    for old_api, old_lineno, old_params in old_api_calls:
-        tmp = []
-        old_parts = old_api.split(".")
-        old_method_name, old_call_chain = old_parts[-1], old_parts[:-1]
-        old_params2 = [p for p in old_params if not p.startswith("#")]
-        is_break = False
-        for i in range(-line_range, line_range + 1):
-            new_lineno = old_lineno + i
-            new_api_params = new_lineno_apicalls.get(new_lineno)
-            if new_api_params is None:
+
+def get_unique_callees(new_callees: list[dict], old_callees: list[dict]) -> list[dict]:
+    unique_callees = []
+    for new_callee in new_callees:
+        new_full_name = new_callee["full_name"]
+        new_arguments = new_callee["arguments"]
+        matched = False
+        for old_callee in old_callees:
+            old_full_name = old_callee["full_name"]
+            old_arguments = old_callee["arguments"]
+            if (old_full_name == new_full_name) and (
+                len(old_arguments) == len(new_arguments)
+            ):
+                matched = True
+                break
+        if not matched:
+            unique_callees.append(new_callee)
+    return unique_callees
+
+
+def get_update_relevant_apis(
+    blob_changes_col: Collection,
+    api_calls_col: Collection,
+    commit: str,
+    dep_update_info: list[dict],
+) -> list:
+    blob_changes = blob_changes_col.find_one(
+        {"commit": commit}, projection={"_id": 0, "blob_changes": 1}
+    )
+    if blob_changes is None:
+        return []
+
+    res = []
+    for blob_change in blob_changes["blob_changes"]:
+        filepath = blob_change["filepath"].strip("/")
+        relevant_packages = relevant_packages_by_filepath(filepath, dep_update_info)
+
+        new_blob = blob_change["new_blob"]
+        new_grouped_api_calls = get_grouped_relevant_api_calls(
+            api_calls_col, new_blob, relevant_packages
+        )
+        if not new_grouped_api_calls:
+            continue
+
+        old_blob = blob_change["old_blob"]
+        old_grouped_api_calls = get_grouped_relevant_api_calls(
+            api_calls_col, old_blob, relevant_packages
+        )
+        if not old_grouped_api_calls:
+            continue
+
+        tmp = {}
+        for caller, new_pkg_callees in new_grouped_api_calls.items():
+            old_pkg_callees = old_grouped_api_calls.get(caller)
+            if old_pkg_callees is None:
                 continue
-            for new_api, new_params in new_api_params:
-                new_params2 = [p for p in new_params if not p.startswith("#")]
-                # param_ratio = ratio(old_params, new_params)
-                new_parts = new_api.split(".")
-                new_method_name, new_call_chain = new_parts[-1], new_parts[:-1]
-                method_name_dis = 1 - ratio(old_method_name, new_method_name)
-                call_chain_dis = 1 - ratio(old_call_chain, new_call_chain)
-                tmp.append(
-                    (
-                        new_api,
-                        new_params,
-                        new_lineno,
-                        method_name_dis,
-                        call_chain_dis,
-                        # param_distance,
-                        abs(i),
-                    )
+            for pkg, new_callees in new_pkg_callees.items():
+                old_callees = old_pkg_callees.get(pkg)
+                if old_callees is None:
+                    continue
+                new_unique_callees = get_unique_callees(new_callees, old_callees)
+                if not new_unique_callees:
+                    continue
+                old_unique_callees = get_unique_callees(old_callees, new_callees)
+                if not old_unique_callees:
+                    continue
+                tmp[pkg] = tmp.get(pkg, [])
+                tmp[pkg].append(
+                    {
+                        "caller": caller,
+                        "new_callees": new_unique_callees,
+                        "old_callees": old_unique_callees,
+                    }
                 )
         if tmp:
-            best_candidate = sorted(tmp, key=lambda x: x[3:])[0]
-            new_api, new_params = best_candidate[:2]
-            if ("(" in old_api) or ("(") in new_api:
-                continue
-            if ("[" in old_api) or ("[") in new_api:
-                continue
-            if old_api == new_api:
-                continue
-            update_instance_candidates.append(
-                (old_api, old_params, old_lineno) + best_candidate[:-1]
-            )
-    return update_instance_candidates
-
-
-def mine_api_updates(
-    new_api_calls: dict[str, list] | None,
-    old_api_calls: dict[str, list] | None,
-    package_imports: dict[str, list[str]],
-):
-    if (new_api_calls is None) or (old_api_calls is None):
-        return
-    new_packages = check_imports(new_api_calls["modules"], package_imports)
-    if not new_packages:
-        return
-    old_packages = check_imports(old_api_calls["modules"], package_imports)
-    if not old_packages:
-        return
-    common_packages = list(
-        set(new_packages.keys()).intersection(set(old_packages.keys()))
-    )
-    if not common_packages:
-        return
-    new_module2packages = {}
-    for k in common_packages:
-        for v in new_packages[k]:
-            new_module2packages[v] = new_module2packages.get(v, [])
-            new_module2packages[v].append(k)
-    old_module2packages = {}
-    for k in common_packages:
-        for v in old_packages[k]:
-            old_module2packages[v] = old_module2packages.get(v, [])
-            old_module2packages[v].append(k)
-
-    new_api_calls = filter_relevant_api_calls(
-        new_api_calls["api_calls"], list(new_module2packages.keys())
-    )
-    if not new_api_calls:
-        return
-    old_api_calls = filter_relevant_api_calls(
-        old_api_calls["api_calls"], list(old_module2packages.keys())
-    )
-    if not old_api_calls:
-        return
-    common_callers = list(
-        set(new_api_calls.keys()).intersection(set(old_api_calls.keys()))
-    )
-
-    res = []
-    for caller in common_callers:
-        update_instance_candidates = candidate_update_instances(
-            new_api_calls[caller], old_api_calls[caller]
-        )
-        if not update_instance_candidates:
-            continue
-        for uic in update_instance_candidates:
-            old_api, new_api = uic[0], uic[3]
-            old_pkgs = []
-            for m, p in old_module2packages.items():
-                if (old_api == m) or (old_api.startswith(f"{m}.")):
-                    old_pkgs = p
-            new_pkgs = []
-            for m, p in new_module2packages.items():
-                if (new_api == m) or (new_api.startswith(f"{m}.")):
-                    new_pkgs = p
-            com_pkgs = list(set(old_pkgs).intersection(set(new_pkgs)))
-            if not com_pkgs:
-                continue
-            for p in com_pkgs:
-                res.append([caller, p, uic])
-    return res
-
-
-def mine_api_updates_batch(batch: list[dict], idx: int):
-    client = MongoClient("127.0.0.1", 27017)
-    db = client["api_update"]
-    col = db["py_api_calls"]
-
-    res = []
-    for doc in batch:
-        commit = doc["commit"]
-        filepath = doc["filepath"]
-        package_imports = {}
-        for p in doc["packages"]:
-            imports = py_imports.get(p)
-            if imports:
-                package_imports[p] = imports
-        blob_pairs = doc["blob_pairs"]
-        for f, nb, ob in blob_pairs:
-            new_api_calls = col.find_one(
-                {"blob_sha": nb}, projection={"_id": 0, "modules": 1, "api_calls": 1}
-            )
-            old_api_calls = col.find_one(
-                {"blob_sha": ob}, projection={"_id": 0, "modules": 1, "api_calls": 1}
-            )
-            apiu = mine_api_updates(new_api_calls, old_api_calls, package_imports)
-            if apiu is None:
-                continue
-            for caller, p, uic in apiu:
+            for pkg, api_call_info in tmp.items():
                 res.append(
                     {
                         "commit": commit,
                         "filepath": filepath,
-                        "file": f,
-                        "old_blob": ob,
-                        "new_blob": nb,
-                        "caller": caller,
-                        "package": p,
-                        "old_api": uic[0],
-                        "old_params": uic[1],
-                        "old_lineno": uic[2],
-                        "new_api": uic[3],
-                        "new_params": uic[4],
-                        "new_lineno": uic[5],
-                        "method_name_dis": uic[6],
-                        "call_chain_dis": uic[7],
-                        # "param_distance": uic[8],
+                        "old_blob": old_blob,
+                        "new_blob": new_blob,
+                        "package": pkg,
+                        "version_before": relevant_packages[pkg]["version_before"],
+                        "version_after": relevant_packages[pkg]["version_after"],
+                        "api_calls": api_call_info,
                     }
                 )
-    with open(f"../benchmark/updates/api_update_instances.json.{idx}", "w") as outf:
+
+    return res
+
+
+def get_nearby_apis_batch(commit_records: list[str], lang: str, idx: int):
+    client = MongoClient("127.0.0.1", 27017)
+    db = client["api_update"]
+    dep_updates_col = db[f"{lang}_dependency_updates"]
+    api_calls_col = db[f"{lang}_api_calls"]
+    blob_changes_col = db["blob_changes"]
+
+    pkg_import_mappings = lang_pkg_import_mappings[lang]
+    res = []
+    for record in commit_records:
+        try:
+            dep_update_info = get_dependency_update_info(
+                dep_updates_col, record[0], pkg_import_mappings
+            )
+
+            for commit in record:
+                if commit == "":
+                    continue
+                try:
+                    tmp = get_update_relevant_apis(
+                        blob_changes_col, api_calls_col, commit, dep_update_info
+                    )
+                    if tmp:
+                        res.extend(tmp)
+                except:
+                    logger.debug(f"get_update_relevant_apis() error for {commit}")
+        except:
+            logger.debug(f"get_dependency_update_info() error for {record[0]}")
+    with open(
+        f"../benchmark/updates/{lang}condensed_api_calls.json.{idx}", "w"
+    ) as outf:
         json.dump(res, outf)
 
 
-def gen_batch(col: Collection, batch_size: int):
+def gen_batch_from_file(filepath: str, batch_size: int):
     batch = []
-    for doc in col.find({}, projection={"_id": 0}):
-        batch.append(doc)
-        if len(batch) == batch_size:
+    with open(filepath) as f:
+        for line in f:
+            batch.append(line.strip("\n").split(","))
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        if batch:
             yield batch
-            batch = []
-    if batch:
-        yield batch
 
 
-def mine_api_updates_all(n_jobs: int = 1, batch_size: int = 1):
+def split_commit_rows():
+    df = pd.read_csv(
+        "../benchmark/updates/c.pc.ppc.cc.ccc",
+        sep=";",
+        names=["commit", "parent", "parent parent", "child", "child child"],
+    )
+    print(f"{len(df)} commits rows")
     client = MongoClient("127.0.0.1", 27017)
     db = client["api_update"]
-    col = db["py_update_blobs"]
-    total_docs = col.count_documents({})
+    for lang in ["py", "java"]:
+        dep_updates_col = db[f"{lang}_dependency_updates"]
+        commits = pd.DataFrame(
+            dep_updates_col.find({}, projection={"_id": 0, "commit": 1})
+        )["commit"].unique()
+        print(f"{lang}: {len(commits)} unique commits")
+        commits = df[df["commit"].isin(commits)]
+        print(len(commits))
+        commits.to_csv(
+            f"../benchmark/updates/{lang}c.pc.ppc.cc.ccc", index=False, header=False
+        )
 
-    num_batches = math.ceil(total_docs / batch_size)
-    batches = gen_batch(col, batch_size)
+
+def get_nearby_apis(lang: str, n_jobs: int = 1, batch_size: int = 1):
+    commit_filepath = f"../benchmark/updates/{lang}c.pc.ppc.cc.ccc"
+    if not os.path.exists(commit_filepath):
+        split_commit_rows()
+
+    total_lines = sum(1 for i in open(commit_filepath, "rb"))
+    num_batches = math.ceil(total_lines / batch_size)
+    print(
+        f"{lang}: {n_jobs} processes",
+        f"{batch_size} blobs/batch",
+        f"{total_lines} lines",
+        f"{num_batches} batches",
+    )
+    batches = gen_batch_from_file(commit_filepath, batch_size)
     Parallel(n_jobs=n_jobs, backend="multiprocessing")(
-        delayed(mine_api_updates_batch)(batch, i)
-        for i, batch in enumerate(tqdm(batches, total=num_batches))
+        delayed(get_nearby_apis_batch)(batch, lang, i)
+        for i, batch in tqdm(enumerate(batches), total=num_batches, file=sys.stdout)
     )
 
 
@@ -258,4 +344,5 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    mine_api_updates_all(args.n_jobs, args.batch_size)
+    get_nearby_apis("py", args.n_jobs, args.batch_size)
+    get_nearby_apis("java", args.n_jobs, args.batch_size)
