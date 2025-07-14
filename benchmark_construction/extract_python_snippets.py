5704fe80f5e5ce58941316b644f7553b4ef692c1
Kai Gao <gaokaipku@qq.com>
2025-07-14 15:25:33 +0800

    Rename code files and implement Python code snippets extraction

--- benchmark_construction/extract_python_snippets.py
+++ benchmark_construction/extract_python_snippets.py
@@ -3,36 +3,37 @@ import os
 from argparse import ArgumentParser
 
 import pandas as pd
+import tree_sitter_python as tspython
 from joblib import Parallel, delayed
-from parse_api_calls import parse_api_calls_java
+from parse_api_calls import parse_api_calls_python, read_raw_blob
 from pymongo import MongoClient
 from tqdm import tqdm
-
-java_imports = json.load(open("../benchmark/updates/java_imports.json"))
-
-import tree_sitter_java as tsjava
-from parse_api_calls import read_raw_blob
 from tree_sitter import Language, Node, Parser
 
-JAVA_LANGUAGE = Language(tsjava.language())
-JAVA_PARSER = Parser(JAVA_LANGUAGE)
+PY_LANGUAGE = Language(tspython.language())
+PY_PARSER = Parser(PY_LANGUAGE)
+
+py_imports = json.load(open("../benchmark/updates/py_imports.json"))
 
 
-method_query = JAVA_LANGUAGE.query(
+py_import_query = PY_LANGUAGE.query(
     """
-[
-    (method_declaration)
-    (constructor_declaration)
-]@declaration
-"""
-)
+(import_statement
+    ([
+        name: (dotted_name) @import_name
+        name: (aliased_import
+            name: (dotted_name) @import_name
+            alias: (identifier) @alias_name)
+    ]))
 
-java_import_query = JAVA_LANGUAGE.query(
-    """
-(import_declaration
-    (identifier) @import_name .)@import_declaration
-(import_declaration
-    (scoped_identifier) @import_name .)@import_declaration
+(import_from_statement
+    module_name: (dotted_name) @from_module
+    ([
+        name: (dotted_name) @import_name
+        name: (aliased_import
+            name: (dotted_name) @import_name
+            alias: (identifier) @alias_name)
+    ]))
 """
 )
 
@@ -40,9 +41,9 @@ java_import_query = JAVA_LANGUAGE.query(
 def select_blob_index():
     client = MongoClient("127.0.0.1", 27017)
     db = client["api_update"]
-    col = db["java_existent_api_update_instances"]
+    col = db["py_candidate_api_update_instances"]
     blob_index = {}
-    with open("../benchmark/updates/javablob.idx") as f:
+    with open("../benchmark/updates/pyblob.idx") as f:
         for line in tqdm(f):
             blob_sha, offset, length = line.strip("\n").split(";")
             idx = int(blob_sha[:2], base=16) % 128
@@ -53,14 +54,14 @@ def select_blob_index():
         blobs.append(doc["new_blob"])
     blobs = list(set(blobs))
     related_blobs_index = {b: blob_index[b] for b in blobs}
-    with open("../benchmark/final/java_blob_index.json", "w") as outf:
+    with open("../benchmark/final/py_blob_index.json", "w") as outf:
         json.dump(related_blobs_index, outf)
 
 
-if not os.path.exists("../benchmark/final/java_blob_index.json"):
+if not os.path.exists("../benchmark/final/py_blob_index.json"):
     select_blob_index()
 
-blob_index = json.load(open("../benchmark/final/java_blob_index.json"))
+blob_index = json.load(open("../benchmark/final/py_blob_index.json"))
 print(f"{len(blob_index)} unique blobs")
 
 
@@ -73,150 +74,119 @@ def extract_spaces(line: str):
     return res
 
 
-def method_caller(caller: list[str]) -> bool:
-    if len(caller) == 0:
+def method_caller(caller: str) -> bool:
+    parts = caller.split(".")
+    if not parts[-1].endswith("()"):
         return False
-
-    # the last caller must be a method
-    if not caller[-1].endswith(")"):
-        return False
-
-    # Outer callers must be either class, enum, record, interface, or annotation_type
-    if any(["@" not in c for c in caller[:-1]]):
+    if any([c.endswith("()") for c in parts[:-1]]):
         return False
     return True
 
 
-def parse_imports_java(root_node: Node):
-    class_mappings = {}
-    for match in java_import_query.matches(root_node):
-        import_declaration = match[1]["import_declaration"][0].text.decode(
-            errors="ignore"
-        )
+def parse_imports_python(root_node: Node):
+    alias_mapping = {}
+
+    for match in py_import_query.matches(root_node):
         import_name = match[1]["import_name"][0].text.decode(errors="ignore")
-        name = import_name.split(".")[-1]
-        if name == "*":
-            continue
-        class_mappings[import_name] = import_declaration
-    return class_mappings
+        alias_name = None
+        if "alias_name" in match[1]:
+            alias_name = match[1]["alias_name"][0].text.decode(errors="ignore")
+        if match[0] == 1:
+            from_module = match[1]["from_module"][0].text.decode(errors="ignore")
+            statement = f"from {from_module} import {import_name}"
+            import_name = f"{from_module}.{import_name}"
+
+        else:
+            statement = f"import {import_name}"
+        if alias_name:
+            statement = statement + f" as {alias_name}"
+        alias_mapping[import_name] = statement
+
+    return alias_mapping
 
 
-def extract_code(
-    source: str, root_node: Node, package: str, caller: list[str]
-) -> str | None:
+def extract_code(source: str, root_node: Node, package: str, caller: str) -> str | None:
     if not method_caller(caller):
         return
 
     source_lines = source.splitlines()
 
-    imports = java_imports[package]
+    imports = py_imports[package]
     related_imports = [
         v
-        for k, v in parse_imports_java(root_node).items()
-        if any(k.startswith(f"{imp}.") for imp in imports)
+        for k, v in parse_imports_python(root_node).items()
+        if any(k.startswith(f"{imp}.") or (k == imp) for imp in imports)
     ]
     cur_node = root_node
     i = 0
+    caller = caller.split(".")
     res = []
-    end_brackets = []
     while cur_node and (i < len(caller)):
         ctx = caller[i]
-        if "@" in ctx:
-            context_type, context_name = ctx.split("@", 1)
-            if context_type not in [
-                "class",
-                "reocrd",
-                "enum",
-                "interface",
-                "annotation_type",
-            ]:
-                return
-            query = JAVA_LANGUAGE.query(f"({context_type}_declaration)@declaration")
+        if ctx.endswith("()"):
+            query = PY_LANGUAGE.query("(function_definition)@definition")
             found = False
             for match in query.matches(cur_node):
-                match_node = match[1]["declaration"][0]
+                match_node = match[1]["definition"][0]
                 node_name = match_node.child_by_field_name("name").text.decode(
                     errors="ignore"
                 )
-                if node_name == context_name:
-                    cur_node = match_node
-                    found = True
-                    start_line = match_node.start_point[0]
-                    leading_spaces = extract_spaces(source_lines[start_line])
-                    res.append(
-                        leading_spaces
-                        + match_node.text.decode(errors="ignore").split("{", 1)[0]
-                        + "{"
-                    )
-                    end_brackets.insert(0, leading_spaces + "}")
-                    i += 1
-                    break
-            if not found:
-                return
-
-        elif ctx.endswith(")"):
-            found = False
-            for match in method_query.matches(cur_node):
-                match_node = match[1]["declaration"][0]
-                node_name = match_node.child_by_field_name("name").text.decode(
-                    errors="ignore"
-                )
-                context_name, context_parameter_types = ctx.split("(", 1)
-                context_parameter_types = context_parameter_types.strip(")")
+                context_name = ctx.split("(", 1)[0]
                 if node_name != context_name:
                     continue
-                method_parameters = match_node.child_by_field_name("parameters")
-                parameter_types = []
-                for param in method_parameters.named_children:
-                    if param.type == "formal_parameter":
-                        t = param.child_by_field_name("type").text.decode(
-                            errors="ignore"
-                        )
-                        parameter_types.append(t)
-                if ", ".join(parameter_types) == context_parameter_types:
-                    found = True
-                    cur_node = match_node
-                    start_line = match_node.start_point[0]
-                    end_line = match_node.end_point[0]
-                    res.append("\n".join(source_lines[start_line : end_line + 1]))
-                    i += 1
-                    break
+                found = True
+                cur_node = match_node
+                start_line = match_node.start_point[0]
+                end_line = match_node.end_point[0]
+                res.append("\n".join(source_lines[start_line : end_line + 1]))
+                i += 1
+                break
+
             if not found:
                 return
         else:
-            query = JAVA_LANGUAGE.query(
-                f"(compact_constructor_declaration)@declaration"
-            )
+            query = PY_LANGUAGE.query("(class_definition)@definition")
             found = False
             for match in query.matches(cur_node):
-                match_node = match[1]["declaration"][0]
+                match_node = match[1]["definition"][0]
                 node_name = match_node.child_by_field_name("name").text.decode(
                     errors="ignore"
                 )
                 if node_name == ctx:
-                    found = True
                     cur_node = match_node
+                    found = True
                     start_line = match_node.start_point[0]
-                    end_line = match_node.end_point[0]
-                    res.append("\n".join(source_lines[start_line : end_line + 1]))
+                    leading_spaces = extract_spaces(source_lines[start_line])
+                    class_header = " ".join(
+                        [
+                            child.text.decode(errors="ignore")
+                            for child in match_node.children[:-1]
+                        ]
+                    )
+                    res.append(leading_spaces + class_header)
                     i += 1
                     break
             if not found:
                 return
 
-    return "\n".join(related_imports + res + end_brackets)
+    return "\n".join(related_imports + res)
 
 
 def extract_arguments(source, api: str):
     api_full_name = api.split("(")[0]
     res = []
-    api_calls = parse_api_calls_java(source).get("api_calls", [])
+    api_calls = parse_api_calls_python(source).get("api_calls", [])
     for api_call in api_calls:
         for callee in api_call["callee"]:
             full_name = callee["full_name"]
             if api_full_name != full_name:
                 continue
-            arguments = [arg["value"] for arg in callee["arguments"]]
+            arguments = []
+            for arg in callee["arguments"]:
+                if "key" in arg:
+                    arguments.append(f"{arg['key']}={arg['value']}")
+                else:
+                    arguments.append(arg["value"])
             res.append(arguments)
     return res
 
@@ -224,7 +194,7 @@ def extract_arguments(source, api: str):
 def extract_code_commit_pair(row):
     client = MongoClient("127.0.0.1", 27017)
     db = client["api_update"]
-    col = db["java_existent_api_update_instances"]
+    col = db["py_candidate_api_update_instances"]
 
     package = row["package"]
     old_api = row["old_api"]
@@ -238,10 +208,10 @@ def extract_code_commit_pair(row):
         version_after = doc["version_after"]
         old_blob = doc["old_blob"]
         old_source = read_raw_blob(*blob_index[old_blob])
-        old_root_node = JAVA_PARSER.parse(old_source).root_node
+        old_root_node = PY_PARSER.parse(old_source).root_node
         new_blob = doc["new_blob"]
         new_source = read_raw_blob(*blob_index[new_blob])
-        new_root_node = JAVA_PARSER.parse(new_source).root_node
+        new_root_node = PY_PARSER.parse(new_source).root_node
         visited_caller = []
         for pair in doc["api_update_pairs"]:
             caller = pair["caller"]
@@ -249,19 +219,11 @@ def extract_code_commit_pair(row):
                 continue
             visited_caller.append(caller)
             old_callee = pair["old_callee"]
-            old_callee_full_name = old_callee["full_name"]
-            if old_callee["parameter_types"] == "":
-                old_callee_params = ""
-            else:
-                old_callee_params = f"({', '.join(old_callee['parameter_types'])})"
-            old_callee_api = old_callee_full_name + old_callee_params
+            old_callee_api = old_callee["full_name"]
+
             new_callee = pair["new_callee"]
-            new_callee_full_name = new_callee["full_name"]
-            if new_callee["parameter_types"] == "":
-                new_callee_params = ""
-            else:
-                new_callee_params = f"({', '.join(new_callee['parameter_types'])})"
-            new_callee_api = new_callee_full_name + new_callee_params
+            new_callee_api = new_callee["full_name"]
+
             if (version_before == old_version) and (version_after == new_version):
                 if (old_callee_api == old_api) and (new_callee_api == new_api):
                     old_code = extract_code(
@@ -333,9 +295,7 @@ def extract_code_commit_pair(row):
 
 
 def extract_main(n_jobs: int = 1):
-    commit_pairs_full = json.load(
-        open("../benchmark/final/java_commit_pairs_full.json")
-    )
+    commit_pairs_full = json.load(open("../benchmark/final/py_commit_pairs_full.json"))
     print(len(commit_pairs_full), "commit pairs for full rules")
 
     res = Parallel(n_jobs=n_jobs, backend="multiprocessing")(
@@ -344,24 +304,24 @@ def extract_main(n_jobs: int = 1):
     final = []
     for r in res:
         final.extend(r)
-    with open("../benchmark/final/java_update_instances_full.json", "w") as outf:
+    with open("../benchmark/final/py_update_instances_full.json", "w") as outf:
         json.dump(final, outf)
 
-    instances_full = pd.read_json("../benchmark/final/java_update_instances_full.json")
+    instances_full = pd.read_json("../benchmark/final/py_update_instances_full.json")
     print(f"{len(instances_full)} update instances for full rules")
-    commit_pairs_exact = pd.read_json("../benchmark/final/java_commit_pairs_exact.json")
+    commit_pairs_exact = pd.read_json("../benchmark/final/py_commit_pairs_exact.json")
     print(f"{len(commit_pairs_exact)} commit pairs for correct verified rules")
     instances_exact = commit_pairs_exact.merge(instances_full)
     print(f"{len(instances_exact)} update instances for correct verified rules")
     instances_exact.to_json(
-        "../benchmark/final/java_update_instances_exact.json", orient="records"
+        "../benchmark/final/py_update_instances_exact.json", orient="records"
     )
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(
-        prog="python extract_java_code.py",
-        description="Extract methods for update pairs",
+        prog="python extract_python_snippets.py",
+        description="Extract code snippets for Python update pairs",
     )
     parser.add_argument("-n", "--n_jobs", type=int, default=1, help="number of workers")
 
